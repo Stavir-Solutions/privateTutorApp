86cb42d8c1570cce958dba78e55f1a5f54f06325
shaniOO3 <official.solo003@gmail.com>
2025-06-01 01:15:35 +0530

    Update NotificationScreen.tsx

--- src/screens/NotificationScreen.tsx
+++ src/screens/NotificationScreen.tsx
@@ -5,21 +5,157 @@ import {
   FlatList,
   TouchableOpacity,
   Platform,
+  Alert,
+  Linking,
+  Animated,
 } from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {notificationz} from '../dumyDb';
+import {getapi, patchApi} from '../utils/api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const NotificationScreen = ({navigation}) => {
-  const [notifications, setNotifications] = useState(notificationz);
+  const [notifications, setNotifications] = useState();
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [fadeAnim] = useState(new Animated.Value(0));
+
+  const animateSuccess = () => {
+    Animated.sequence([
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+      Animated.delay(2000),
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
 
   const handleNotificationPress = notification => {
     const updatedNotifications = notifications.map(item =>
       item.id === notification.id ? {...item, seen: true} : item,
     );
+    console.log(notification);
+    Notification_marking(notification.id);
     setNotifications(updatedNotifications);
+
+    if (notification.deeplink && notification.type) {
+      const urlParts = notification.deeplink.split('/');
+      const objectId = urlParts[urlParts.length - 1];
+
+      switch (notification.type) {
+        case 'MESSAGE':
+          navigation.navigate('Chat', {
+            conversationId: objectId,
+            deeplink: true,
+          });
+          break;
+        case 'ASSIGNMENT':
+          navigation.navigate('Assignment_Detail', {
+            assignmentId: objectId,
+            deeplink: true,
+          });
+          break;
+        case 'NOTE':
+          navigation.navigate('Note_Detail', {
+            noteId: objectId,
+            deeplink: true,
+          });
+          break;
+        case 'FEE':
+          navigation.navigate('Fees_Detail', {feeId: objectId, deeplink: true});
+          break;
+        case 'STUDENT':
+          navigation.navigate('Student_Detail', {
+            studentId: objectId,
+            deeplink: true,
+          });
+          break;
+        default:
+          const path = urlParts[urlParts.length - 2];
+          if (path === 'messages') {
+            navigation.navigate('Chat', {
+              conversationId: objectId,
+              deeplink: true,
+            });
+          }
+
+          break;
+      }
+    }
   };
+
+  const Notification_marking = async (notification_id: any) => {
+    try {
+      const Token = await AsyncStorage.getItem('Token');
+
+      const url = `/notifications/${notification_id}/seen`;
+      const headers = {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${Token}`,
+      };
+
+      const onResponse = res => {
+        setShowSuccessMessage(true);
+        animateSuccess;
+        console.log(res);
+        console.log('notification marked');
+        setTimeout(() => {
+          setShowSuccessMessage(false);
+        }, 2000);
+      };
+
+      const onCatch = error => {
+        console.error('Error in notification marking:', error);
+        Alert.alert('Error', 'Failed to mark notification. Please try again.', [
+          {text: 'OK'},
+        ]);
+      };
+
+      patchApi(url, headers, null, onResponse, onCatch, navigation);
+    } catch (error) {
+      console.error('Error marking notification:', error);
+      Alert.alert('Error', 'Failed to mark notification. Please try again.', [
+        {text: 'OK'},
+      ]);
+    }
+  };
+
+  const Notification_fetch = async () => {
+    const Token = await AsyncStorage.getItem('Token');
+    const Teacher_id = await AsyncStorage.getItem('TeacherId');
+
+    const url = `notifications/teachers/${Teacher_id}`;
+    const headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${Token}`,
+    };
+    const onResponse = res => {
+      console.log('hiii');
+      console.log(Teacher_id);
+      console.log('res', res);
+      setNotifications(res);
+    };
+
+    const onCatch = res => {
+      console.log('Error');
+      console.log(res);
+    };
+    getapi(url, headers, onResponse, onCatch, navigation);
+  };
+
+  useEffect(() => {
+    Notification_fetch();
+  }, [1]);
 
   const renderNotificationCard = ({item}) => (
     <TouchableOpacity
@@ -33,14 +169,30 @@ const NotificationScreen = ({navigation}) => {
           styles.iconContainer,
           {backgroundColor: `${item.iconColor}15`},
         ]}>
-        <MaterialIcons name={item.icon} size={24} color={item.iconColor} />
+        {item.type === 'MESSAGE' ? (
+          <MaterialIcons name="message" size={24} color="#4CAF50" />
+        ) : item.type === 'FEE_PAID' ? (
+          <MaterialIcons name="payments" size={24} color="#FF9800" />
+        ) : (
+          <MaterialCommunityIcons
+            name="account-plus-outline"
+            size={24}
+            color="#2196F3"
+          />
+        )}
       </View>
       <View style={styles.notificationContent}>
         <View style={styles.notificationHeader}>
-          <Text style={styles.notificationTitle}>{item.title}</Text>
+          <Text style={styles.notificationTitle}>
+            {item.type === 'MESSAGE'
+              ? 'NEW MESSAGE'
+              : item.type === 'FEE_PAID'
+              ? 'FEE PAID'
+              : 'NEW STUDENT'}
+          </Text>
           <Text style={styles.timeText}>{item.time}</Text>
         </View>
-        <Text style={styles.notificationDescription}>{item.description}</Text>
+        <Text style={styles.notificationDescription}>{item.title}</Text>
       </View>
       {!item.seen && <View style={styles.unreadDot} />}
     </TouchableOpacity>
@@ -55,14 +207,28 @@ const NotificationScreen = ({navigation}) => {
         <Text style={styles.appBarTitle}>Notifications</Text>
         <MaterialIcons name="arrow-back" size={24} color="rgba(0, 0, 0, 0)" />
       </View>
+      {showSuccessMessage && (
+        <Animated.View style={[styles.successMessage, {opacity: fadeAnim}]}>
+          <MaterialIcons name="check-circle" size={24} color="#059669" />
+          <Text style={styles.successText}>{'Notification read marked'}</Text>
+        </Animated.View>
+      )}
 
-      <FlatList
-        data={notifications}
-        renderItem={renderNotificationCard}
-        keyExtractor={item => item.id}
-        contentContainerStyle={styles.notificationList}
-        showsVerticalScrollIndicator={false}
-      />
+      {notifications && notifications?.length > 0 ? (
+        <FlatList
+          data={notifications}
+          renderItem={renderNotificationCard}
+          keyExtractor={item => item.id}
+          contentContainerStyle={styles.notificationList}
+          showsVerticalScrollIndicator={false}
+        />
+      ) : (
+        <View style={styles.noNotificationContainer}>
+          <Text style={styles.noNotificationText}>
+            No notifications in this batch
+          </Text>
+        </View>
+      )}
     </View>
   );
 };
@@ -100,7 +266,7 @@ const styles = StyleSheet.create({
     shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.1,
     shadowRadius: 4,
-    elevation: 3,
+    elevation: 12,
   },
   unreadNotification: {
     backgroundColor: '#F8FAFF',
@@ -146,6 +312,32 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 16,
     right: 16,
+  },
+  successMessage: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#ECFDF5',
+    padding: 16,
+    marginHorizontal: 20,
+    marginTop: 20,
+    borderRadius: 12,
+  },
+  successText: {
+    marginLeft: 8,
+    color: '#059669',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  noNotificationContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  noNotificationText: {
+    fontSize: 18,
+    color: '#888',
+    textAlign: 'center',
   },
 });
 
